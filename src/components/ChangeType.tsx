3d0bc35e460d4754bd0d6920c2de118ea731c096
JCyr7 <jackson.cyr@maine.edu>
2022-10-24 12:18:59 -0400

    change type working

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [qType, setqType] = useState<QuestionType>("short_answer_question");
+
+    function flipVisibility(): void {
+        // Set visible to be the logical opposite of its previous value
+        if (qType === "multiple_choice_question") {
+            setqType("short_answer_question");
+        } else {
+            setqType("multiple_choice_question");
+        }
+    }
+
+    return (
+        <span>
+            <Button onClick={flipVisibility}>Change Type</Button>
+            {qType === "multiple_choice_question" && <div>Multiple Choice</div>}
+            {qType === "short_answer_question" && <div>Short Answer</div>}
+        </span>
+    );
 }
